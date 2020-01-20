b66da3457b5df145fe4d06e898480229afec9a58
Elen Joseph <jose0205@algonquinlive.com>
2020-01-20 17:52:10 -0500

    finished

--- script.js
+++ script.js
@@ -56,23 +56,24 @@ const game = {
 // shufle the cards
 //deal the cards to the players
 //slice method
-deck.slice(0,13)
+//deck.slice(0,13)
 //loop method
-for (const player in game.players){
-  for (let i=0;i<13; i++){
-    game.players[players]=deck.shift()
-  }
-}
+//for (const player in game.players){
+ // for (let i=0;i<13; i++){
+   // game.players[players]=deck.shift()
+ // }
+//}
 //alternet players
-for(let i = 0; i < deck.lenght;i++){
-  game.players['Players 1'].push(game.deck[i])
-  game.players['Players 2'].push(game.deck[++i])
-}
+//for(let i = 0; i < deck.lenght;i++){
+  //game.players['Players 1'].push(game.deck[i])
+  //game.players['Players 2'].push(game.deck[++i])
+//}
 //playess show their hands
 // create players button
 //create event listener to the parent
 //use button textContent to get player index
 //
+
 const cards =[]
 const hand = document.getElementById('hand')
 for ( const suit of suits ){
@@ -81,19 +82,39 @@ for (const rank of ranks){
   cards.push({ suit,rank
   })
 }
- }
- const shuffled =shuffle(cards)
- hand.innerHTML =shuffled.map(function(card){
-   return `<div class="card">
-   <div class="number ${card.suit}">${card.rank}</div>
-   <div class="${card.suit}"><img src="images/${card.suit}.png"></div>
-   <div class="number ${card.suit}">${card.rank}</div>
- </div>`
- }).join('') 
+}
 
- 
+game.deck =shuffle(cards)
 
- 
+//alternet players
 
- 
+for(let i = 0; i < game.deck.length;i++){
+  game.players['Player 1'].push(game.deck[i])
+  game.players['Player 2'].push(game.deck[++i])
+  game.players['Player 3'].push(game.deck[++i])
+  game.players['Player 4'].push(game.deck[++i])
+}
 
+// create players button
+
+const buttons = []
+for (const player in game.players){
+  buttons.push(`<button class="button">${player}</button>`)
+}
+const $controls = document.getElementById('controls')
+$controls.innerHTML = buttons.join('')
+
+//create event listener to the parent
+$controls.addEventListener('click', function(e){
+  if (e.target.classList.contains('button')){
+    hand.innerHTML=game.players[e.target.textContent].map(
+      function(card){
+        return `<div class="card">
+        <div class="number ${card.suit}">${card.rank}</div>
+        <div class="suit"><img src="images/${card.suit}.png"></div>
+        <div class="number ${card.suit}">${card.rank}</div>
+      </div>`}).join('')
+    
+      }
+  }
+)
